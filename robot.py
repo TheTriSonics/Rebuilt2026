35d1094355d93eabb1c048a96a1ae040cf6efb8e
Student Coder <studentcoder4003@gmail.com>
2026-02-27 19:04:22 -0500

    Revert "Merge pull request #5 from TheTriSonics/simballs-254ideas"
    This reverts commit 5d7ed434cf82832e05e4b2d122684c0624f68e77, reversing
    changes made to 79788cbcd587ca537ef46adaa7875abf35cd0540.

--- robot.py
+++ robot.py
@@ -80,7 +80,6 @@ class MyRobot(MagicRobot):
             wpilib.DriverStation.silenceJoystickConnectionWarning(True)
 
     def autonomousInit(self):
-        self.drivetrain.disable_heading_lock()
         self.tanker.engage()
         selected = self.auton_chooser.getSelected()
         if selected:
@@ -102,7 +101,6 @@ class MyRobot(MagicRobot):
             self.gaspump.engage()
         self.tanker.go_drive_field()
         self.turret.set_hub_target()
-        self.drivetrain.enable_heading_lock()
 
     def teleopPeriodic(self):
         if self.battery_monitor.is_stop_active():
